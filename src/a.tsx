d3fe8b9473582a936342d88254892522af5cb4d3
plume zhou <jmyumao@126.com>
2024-01-29 16:46:36 +0800

    use routes config & await perf

--- src/a.tsx
+++ src/a.tsx
@@ -1,19 +1,50 @@
-import { useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import {
+  useLoaderData,
+  Link,
+  Await,
+  useAsyncValue,
+  useAsyncError,
+  defer,
+} from "react-router-dom";
+import { Skeleton } from "./components/ui/skeleton";
 
 export async function loader() {
-  await new Promise((r) => setTimeout(r, 5500));
-  return "I came from the A.tsx loader function!";
+  // await new Promise((r) => setTimeout(r, 5500));
+  const data = new Promise((r) => setTimeout(() =>r("I came from the A.tsx loader function!"), 5500));
+  return defer({
+    data,
+  });
 }
 
 export function Component() {
-  const data = useLoaderData() as string;
-
+  const { data } = useLoaderData() as { data: Promise<string> };
+  console.log(data)
   return (
     <div>
       <h2>About</h2>
-      <p>{data}</p>
+      <Suspense fallback={<Skeleton className="h-10 w-40" />}>
+        <Await resolve={data} errorElement={<ReviewsError />}>
+          <Reviews />
+        </Await>
+        {/* <Await
+          resolve={data}
+          errorElement={<div>Could not load reviews 😬</div>}
+          children={(data) => <p>{data}</p>}
+        /> */}
+      </Suspense>
+      <Link to="/about">About</Link>
     </div>
   );
 }
 
+function ReviewsError() {
+  const error = useAsyncError() as { message: string };
+  return <div>{error.message}</div>;
+}
+
+function Reviews() {
+  const data = useAsyncValue() as string;
+  return <div>{data}</div>;
+}
 Component.displayName = "APage";
--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,9 @@ import {
   Route,
   Link,
   Outlet,
+  redirect,
+  useNavigate,
+  type RouteObject,
 } from "react-router-dom";
 import React, { Suspense } from "react";
 // import ContactForm from "./pages/concat";
@@ -32,67 +35,165 @@ const Layout = () => {
 //   return <div className="text-red-500 text-3xl">404</div>;
 // };
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
-      <Route
-        index
-        element={
+const Login = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => {
+        sessionStorage.setItem("isLogin", "true");
+        navigate("/home");
+      }}
+    >
+      login
+    </div>
+  );
+};
+
+const AppRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: (
           <Suspense fallback={<div className="loading">Loading...</div>}>
             <HomeRute />
           </Suspense>
-        }
-      ></Route>
-      <Route
-        path="about"
-        element={
+        ),
+      },
+      {
+        path: "about",
+        element: (
           <div className="flex gap-4">
-            About<Link to="/">Index</Link>
+            <Link to="/">Index</Link>
             <Link to="/home">Home</Link>
             <Link to="/contacts">Concat</Link>
           </div>
-        }
-      ></Route>
-      <Route path="home" lazy={() => import("./a")}></Route>
-      <Route
-        path="contacts"
-        element={<ContactRoot />}
-        errorElement={<ErrorPage />}
-        loader={rootLoader}
-        action={rootAction}
-      >
-        <Route errorElement={<ErrorPage />}>
-          <Route index element={<ContactHome />} />
-          <Route
-            path=":contactId"
-            element={<Contact />}
-            loader={contactLoader}
-            action={contactAction}
-          />
-          <Route
-            path=":contactId/edit"
-            element={<EditContact />}
-            loader={contactLoader}
-            action={editAction}
-          />
-          <Route
-            path=":contactId/destroy"
-            action={destroyAction}
-            errorElement={<div>Oops! There was an error.</div>}
-          />
-        </Route>
+        ),
+      },
+      {
+        path: "home",
+        // lazy: () => import("./a"),
+        async lazy() {
+          const { Component, loader } = await import("./a");
+          return {
+            loader,
+            Component,
+          };
+        },
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        loader: async (args) => {
+          const isLogin = sessionStorage.getItem("isLogin");
+          console.log(args, isLogin);
+          if (!isLogin) {
+            return redirect(`/login`);
+          }
+          return null;
+        },
+        children: [
+          {
+            path: "contacts",
+            element: <ContactRoot />,
+            errorElement: <ErrorPage />,
+            loader: rootLoader,
+            action: rootAction,
+            children: [
+              {
+                errorElement: <ErrorPage />,
+                children: [
+                  {
+                    index: true,
+                    element: <ContactHome />,
+                  },
+                  {
+                    path: ":contactId",
+                    element: <Contact />,
+                    loader: contactLoader,
+                    action: contactAction,
+                  },
+                  {
+                    path: ":contactId/edit",
+                    element: <EditContact />,
+                    loader: contactLoader,
+                    action: editAction,
+                  },
+                  {
+                    path: ":contactId/destroy",
+                    action: destroyAction,
+                    errorElement: <div>Oops! There was an error.</div>,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+const router = createBrowserRouter(AppRoutes);
+
+// @ts-expect-error ts(2322)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = createRoutesFromElements(
+  <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+    <Route
+      index
+      element={
+        <Suspense fallback={<div className="loading">Loading...</div>}>
+          <HomeRute />
+        </Suspense>
+      }
+    ></Route>
+    <Route
+      path="about"
+      element={
+        <div className="flex gap-4">
+          About<Link to="/">Index</Link>
+          <Link to="/home">Home</Link>
+          <Link to="/contacts">Concat</Link>
+        </div>
+      }
+    ></Route>
+    <Route path="home" lazy={() => import("./a")}></Route>
+    <Route
+      path="contacts"
+      element={<ContactRoot />}
+      errorElement={<ErrorPage />}
+      loader={rootLoader}
+      action={rootAction}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<ContactHome />} />
+        <Route
+          path=":contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path=":contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path=":contactId/destroy"
+          action={destroyAction}
+          errorElement={<div>Oops! There was an error.</div>}
+        />
       </Route>
-      {/* <Route path="*" element={<NotFound />} /> */}
     </Route>
-  )
+    {/* <Route path="*" element={<NotFound />} /> */}
+  </Route>
 );
-//   {
-//     path: "about",
-//     element: <div>About
-//       <Link to="/">Index</Link>
-//     </div>,
-//   },
-// ]);
 
 const App = () => {
   return <RouterProvider router={router} />;
